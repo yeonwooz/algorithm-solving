6ffccc661c19a1e09b7756d93343e668f5038d08
Suyeon Woo <sean123@pubg.com>
2026-04-02 10:53:54 +0900

    템플릿 readline으로 변경

--- js/js-template.js
+++ js/js-template.js
@@ -1,28 +1,22 @@
-import fs from 'fs';
+const readline = require('readline');
+
+main();
+
+async function main() {
+    const inputs = await getInputs();
+    console.log(inputs);
+}
 
 function getInputs() {
-    const input = fs.readFileSync('./input.txt', 'utf-8');
-    const lines = input.split('\r\n');
-    const firstLine = lines.shift();
+    return new Promise((resolve) => {
+        const rl = readline.createInterface({ input: process.stdin });
 
-    let arr1 = [];
-    let arr2 = [];
-
-    const [n, m] = firstLine.split(' ').map(Number);
-    const firstElements = lines.slice(0, n);
-    const secondElements = lines.slice(n);
-
-    const aMatrix = makeMatrix(n, m, firstElements);
-    const bMatrix = makeMatrix(n, m, secondElements);
+        const lines = [];
+        rl.on('line', (line) => {
+            lines.push(line);
+        });
+        rl.on('close', () => {
+            resolve(lines);
+        });
+    });
 }
-
-function makeMatrix(n, m, arr) {
-    const matrix = [];
-    for (let i = 0; i < n; i++) {
-        const elements = arr[i].split(' ').map(Number);
-        matrix.push(elements);
-    }
-    return matrix;
-}
-
-getInputs();
