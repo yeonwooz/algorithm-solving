980c4023fa1ed8a82602940c2f6cf76abf98fe34
Suyeon Woo <sean123@pubg.com>
2026-04-02 10:57:45 +0900

    24416 최적화

--- js/24416.js
+++ js/24416.js
@@ -8,30 +8,12 @@ async function main() {
 }
 
 function solve(n) {
-    let count1 = 0;
-    let count2 = 0;
-
-    function fib(n) {
-        if (n === 1 || n === 2) {
-            count1++;
-            return 1;
-        }
-        return fib(n - 1) + fib(n - 2);
+    const dp = new Array(n + 1).fill(1);
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
     }
 
-    function fibonacci(n) {
-        const dp = new Array(n + 1).fill(1);
-        for (let i = 3; i <= n; i++) {
-            count2++;
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        return dp[n];
-    }
-
-    fib(n);
-    fibonacci(n);
-
-    return `${count1} ${count2}`;
+    return `${dp[n]} ${n - 2}`;
 }
 
 function getInputs() {
